54ac218b9eed04db0c9110da2c9418bc68c8027a
onichandame <zxinmyth@gmail.com>
2020-06-03 06:55:03 +0000

    type fixed

--- src/useSysMode.tsx
+++ src/useSysMode.tsx
@@ -1,24 +1,19 @@
-import { useReducer, useEffect } from 'react';
-import { object, boolean, InferType } from 'yup';
+import { useReducer, useEffect } from "react"
 
-const SysModeSchema = object().shape({
-  dark: boolean()
-    .required()
-    .default(false),
-});
-
-type SysMode = InferType<typeof SysModeSchema>;
+type SysMode = {
+  dark: boolean
+}
 
 export const useSysMode = (): SysMode => {
   const [dark, updateDark] = useReducer(
     () =>
       window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches,
+      window.matchMedia("(prefers-color-scheme: dark)").matches,
     false
-  );
+  )
   useEffect(() => {
-    updateDark();
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(updateDark);
-  }, []);
-  return { dark };
-};
+    updateDark()
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(updateDark)
+  }, [])
+  return { dark }
+}
